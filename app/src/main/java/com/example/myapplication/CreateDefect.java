17fbd47088617d29d803d2f9328e2e1868cc8215
Asus <ustimenkoandrej92@gmail.com>
2026-01-28 22:27:38 +0300

    trying28.01.26

---
+++ app/src/main/java/com/example/myapplication/CreateDefect.java
@@ -0,0 +1,167 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CreateDefect extends AppCompatActivity {
+    EditText type, plase;
+    Button add2, delete2, update2, test2;
+    DatabaseHealper myDb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_defect);
+        myDb = new DatabaseHealper(this);
+
+        type = (EditText)findViewById(R.id.type);
+        plase =(EditText)findViewById(R.id.plase);
+        //int id = ;
+
+        add2 = (Button)findViewById(R.id.btn_add2);
+        update2 = (Button)findViewById(R.id.btn_update2);
+        delete2 = (Button)findViewById(R.id.btn_delete2);
+
+        test2 = (Button)findViewById(R.id.btn_test2);
+
+        if(getIntent().getStringExtra("ITEM_ID") != null){
+            LoadItemData();
+        }
+        AddData();
+        updateData();
+        Delete();
+        Read();
+
+    }
+
+
+    public void LoadItemData(){
+        String targetId = getIntent().getStringExtra("ITEM_ID");
+
+        Cursor res = myDb.getDataById2(targetId);
+        if(res.getCount() == 0) {
+            Toast.makeText(this, "ID " + targetId + " не найден", Toast.LENGTH_SHORT).show();
+            res.close();
+            return;
+        }
+
+        if(res.moveToFirst()) {
+            //id.setText(res.getString(0));
+            plase.setText(res.getString(1));
+            plase.setText(res.getString(2));
+        }
+        res.close();
+    }
+
+
+    public void goToLayoutMain(View view) {
+        finish(); // Закрыть CreateObgect → покажет MainActivity
+    }
+    public void goBack(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+    public void Read(){
+        test2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String targetId = id.getText().toString().trim();
+                        Cursor res = myDb.getDataById2(targetId);
+                        if(res.getCount() == 0) {
+                            ShowMassage("Error", "ID " + targetId + " не найден");
+                            return;
+                        }
+
+                        if(res.moveToFirst()) {
+                            id.setText(res.getString(0));
+                            type.setText(res.getString(1));
+                            plase.setText(res.getString(2));
+                        }
+                        res.close();
+                    }
+                }
+        );
+    }
+
+    public void AddData(){
+        add2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = myDb.insertData2(type.getText().toString(), plase.getText().toString());
+                        if(isInserted == true){
+                            Toast.makeText(com.example.myapplication.CreateDefect.this, "Saved", Toast.LENGTH_LONG).show();
+                        } else{
+                            Toast.makeText(com.example.myapplication.CreateDefect.this, "Not saved", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+        );
+    }
+
+
+
+    public void ShowMassage(String title, String massage){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(massage);
+        builder.show();
+    }
+
+
+
+    public void updateData(){
+        update2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isUpdated = myDb.updataData2(id.getText().toString(), type.getText().toString(), plase.getText().toString());
+
+                        if(isUpdated){
+                            Toast.makeText(com.example.myapplication.CreateDefect.this, "Updated", Toast.LENGTH_LONG).show();
+                        } else{
+                            Toast.makeText(com.example.myapplication.CreateDefect.this, "Not Updated", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+        );
+    }
+
+    public void Delete(){
+        delete.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Integer isDeleted = myDb.deleteData2(id.getText().toString());
+
+                        if(isDeleted > 0){
+                            Toast.makeText(com.example.myapplication.CreateDefect.this, "Deleted", Toast.LENGTH_LONG).show();
+                            goToLayoutMain(v);
+                        } else{
+                            Toast.makeText(com.example.myapplication.CreateDefect.this, "Not Deleted", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+        );
+    }
+
+
+
+}
