6e40406ffc72963bc99d51196522a016756b4043
Asus <ustimenkoandrej92@gmail.com>
2026-01-29 13:47:53 +0300

    first ver

---
+++ app/src/main/java/com/example/myapplication/Defects_Adapter.java
@@ -0,0 +1,86 @@
+package com.example.myapplication;
+
+import android.content.ClipData;
+import android.content.Context;
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Item_Adapter_Defects extends RecyclerView.Adapter<Item_Adapter.MyViewHolder> {
+
+    private Context context;
+    private ArrayList<String> id,name;
+
+    private OnItemClickListener listener;
+    public interface OnItemClickListener{
+        void onItemClick(int position);
+    }
+
+
+    Item_Adapter_Defects(Context context, ArrayList id, ArrayList name){
+        this.context = context;
+        this.id = id;
+        this.name = name;
+    }
+
+
+
+    @NonNull
+    @Override
+    public Item_Adapter_Defects.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.item_list, parent, false);
+
+        MyViewHolder holder = new MyViewHolder(view);
+
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Item_Adapter.MyViewHolder holder, int position) {
+        holder.id.setText(String.valueOf(id.get(position)));
+        holder.name.setText(String.valueOf(name.get(position)));
+    }
+
+    @Override
+    public int getItemCount() {
+        return id.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView id, name;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            id = itemView.findViewById(R.id.text_id);
+            name = itemView.findViewById(R.id.text_name);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if(position != RecyclerView.NO_POSITION && listener != null){
+                        listener.onItemClick(position);
+                    }
+                }
+            });
+        }
+    }
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+}
